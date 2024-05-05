f799d16d095ac8999ed698b18006efaecc45ecc8
kzhrk <info@kzhrk.com>
2024-05-05 11:03:02 +0900

    update Metadata type

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,14 +1,4 @@
 interface Metadata {
-	actorIds: string[];
-	audioFilePath: string;
-	audioFileSize: string;
-	date: string;
-	description: string;
-	duration: string;
-	layout: string;
 	title: string;
-	chapters?: {
-		value: string;
-		time: string;
-	}[];
+	date: string;
 }
