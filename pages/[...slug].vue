9ef142e1683372e9aeeba477888c14ea7bf08cb2
kzhrk <info@kzhrk.com>
2024-05-09 21:42:15 +0900

    update 404 logo link

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="p-6 sm:p-12 text-center">
 		<figure>
-			<a href="https://github.com/SAWARATSUKI/ServiceLogos" class="block w-full sm:w-1/2 mx-auto">
+			<a href="https://github.com/SAWARATSUKI/KawaiiLogos" class="block w-full sm:w-1/2 mx-auto">
 				<img src="/images/404.webp" width="320" height="180" alt="404 NOT FOUND" class="w-full">
 			</a>
 		</figure>
