3c67b97ad8c3557eca485e156d772e6213ba9943
kzhrk <info@kzhrk.com>
2024-05-11 23:39:18 +0900

    impl feat draft

--- server/api/posts/index.ts
+++ server/api/posts/index.ts
@@ -3,13 +3,18 @@ import parseMD from "parse-md";
 
 export default defineEventHandler(async () => {
 	const posts = readdirSync("./posts").reverse();
-	return posts.map((p) => {
-		const path = p.replace(/(\d+)-(\d+)-(\d+)-([\w|-]+)\.md/, "/$1/$2/$3/$4");
-		const file = readFileSync(`./posts/${p}`);
-		const { metadata } = parseMD(file.toString()) as { metadata: Metadata };
-		return {
-			...metadata,
-			path,
-		};
-	});
+	return posts
+		.map((p) => {
+			const path = p.replace(/(\d+)-(\d+)-(\d+)-([\w|-]+)\.md/, "/$1/$2/$3/$4");
+			const file = readFileSync(`./posts/${p}`);
+			const { metadata } = parseMD(file.toString()) as { metadata: Metadata };
+
+			if (metadata.draft) return null;
+
+			return {
+				...metadata,
+				path,
+			};
+		})
+		.filter((p) => p);
 });
--- types/index.d.ts
+++ types/index.d.ts
@@ -2,4 +2,5 @@ interface Metadata {
 	title: string;
 	date: string;
 	tags?: string[];
+	draft?: boolean;
 }
