954bebee6c830d60213b9102c06afddee16cfeb9
kzhrk <info@kzhrk.com>
2025-01-19 10:43:04 +0900

    chore: 記事詳細のスタイル修正
    - table の偶数行に背景色を追加
    - heading のアンカーリンクの位置を修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,56 +2,63 @@ import { readdirSync } from "node:fs";
 
 const posts = readdirSync("./posts");
 const routes = posts.map((p) =>
-	p.replace(/(\d+)-(\d+)-(\d+)-([\w|-]+)\.md/, "$1/$2/$3/$4"),
+				p.replace(/(\d+)-(\d+)-(\d+)-([\w|-]+)\.md/, "$1/$2/$3/$4"),
 );
 
 export default defineNuxtConfig({
-	app: {
-		head: {
-			htmlAttrs: {
-				lang: "ja",
-			},
-			script: [
-				{
-					innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-					})(window,document,'script','dataLayer','GTM-5WPKT2H7');`,
-				},
-				{
-					type: "text/javascript",
-					async: true,
-					src: "https://platform.twitter.com/widgets.js",
-					tagPosition: "bodyClose",
-				},
-			],
-		},
+ app: {
+					head: {
+									htmlAttrs: {
+													lang: "ja",
+									},
+									script: [
+													{
+																	innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+																	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+																	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+																	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+																	})(window,document,'script','dataLayer','GTM-5WPKT2H7');`,
+													},
+													{
+																	type: "text/javascript",
+																	async: true,
+																	src: "https://platform.twitter.com/widgets.js",
+																	tagPosition: "bodyClose",
+													},
+									],
+					},
 	},
-	ssr: true,
-	modules: [
-		"@nuxtjs/tailwindcss",
-		"@nuxtjs/sitemap",
-		"@nuxt/test-utils/module",
+
+ ssr: true,
+
+ modules: [
+					"@nuxtjs/tailwindcss",
+					"@nuxtjs/sitemap",
+					"@nuxt/test-utils/module",
 	],
-	generate: {
-		routes,
+
+ generate: {
+					routes,
 	},
-	site: {
-		url: "https://blog.kzhrk.com",
-		trailingSlash: false,
+
+ site: {
+					url: "https://blog.kzhrk.com",
+					trailingSlash: false,
 	},
-	vite: {
-		plugins: [
-			{
-				name: "markdown-loader",
-				transform(code, id) {
-					if (id.slice(-3) === ".md") {
-						// For .md files, get the raw content
-						return `export default ${JSON.stringify(code)};`;
-					}
-				},
-			},
-		],
+
+ vite: {
+					plugins: [
+									{
+													name: "markdown-loader",
+													transform(code, id) {
+																	if (id.slice(-3) === ".md") {
+																					// For .md files, get the raw content
+																					return `export default ${JSON.stringify(code)};`;
+																	}
+													},
+									},
+					],
 	},
+
+ compatibilityDate: "2025-01-19",
 });
